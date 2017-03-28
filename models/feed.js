f2a89e2fcf7628649e5966cc2b7e2e9d1c3c06c0
sega4revenge <sega4revenge@gmail.com>
2017-03-28 14:35:24 +0700

    asd

--- models/feed.js
+++ models/feed.js
@@ -27,4 +27,4 @@ const feedSchema = mongoose.Schema({
 mongoose.Promise = global.Promise;
 mongoose.createConnection('mongodb://developserver.ga:27017/quickjobfind');
 
-module.exports = mongoose.model('feeds',feedSchema);
+module.exports = mongoose.model('feed',feedSchema);
