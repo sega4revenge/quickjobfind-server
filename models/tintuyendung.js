5ae996f43cfddf114712b22454c4971a7c7e2e72
Sega <sega4revenge@gmail.com>
2017-05-31 15:45:19 +0700

    hjfgdfh

--- models/tintuyendung.js
+++ models/tintuyendung.js
@@ -5,7 +5,7 @@
 
 const mongoose = require("./connect");
 const tintuyendungSchema = mongoose.Schema({
-    _id : String,
+
     macv : String,
     matd             : String,
     tencv            : String,
