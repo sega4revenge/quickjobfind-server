92aa11e360d13e82fedb9cc1f75b6c8cf023732c
LINH <hohoanglinh190295@gmail.com>
2017-08-03 14:00:37 +0700

    haha

--- routes.js
+++ routes.js
@@ -171,7 +171,7 @@ module.exports = router => {
         form.parse(req, (err, fields, files) => {
             if (err) return res.status(500).json({ error: err });
             console.log(files.image.path);
-            res.status(200).json({ uploaded: true , name : fields.name})
+            res.status(200).json({ uploaded: true , name : fields.iduser})
         });
         form.on('fileBegin', function (name, file) {
             const [fileName, fileExt] = file.name.split('.');
