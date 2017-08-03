5b5466072bde4708c1a314c3d7043b2e7d342e7f
sega4 <sega4revenge@gmail.com>
2017-08-03 21:07:05 +0700

    asd

--- models/product.js
+++ models/product.js
@@ -6,7 +6,6 @@ const Schema = require("mongoose/lib/schema");
 
 
 const productSchema = mongoose.Schema({
-    productid : { type: Schema.Types.ObjectId, unique: true },
     productname             : String,
     iduser             : { type: Schema.Types.ObjectId, ref: 'user' },
     price            : String,
