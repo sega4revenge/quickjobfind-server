20f16dcab9c8efb9d805c3f8feb219e604c419e7
sega4 <sega4revenge@gmail.com>
2017-08-03 15:12:34 +0700

    asd

--- functions/profile.js
+++ functions/profile.js
@@ -43,7 +43,11 @@ exports.getProfile = userid =>
         let ObjectId;
         ObjectId = require('mongodb').ObjectID;
         product.find({})
-            .populate('iduser')
+            .populate({
+                path: 'iduser',
+                match: { iduser: ObjectId(userid)}
+
+            })
             .limit(5)
             .then(products => resolve(products[0]))
             .catch(err => reject({ status: 500, message: 'Internal Server Error !' }))
