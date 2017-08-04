60092170d06047f343a68290b58a4e94ca4e280f
LINH <hohoanglinh190295@gmail.com>
2017-08-04 11:50:02 +0700

    haha

--- routes.js
+++ routes.js
@@ -221,7 +221,7 @@ module.exports = router => {
         form.uploadDir = uploadDir;
         form.parse(req, (err, fields, files) => {
             if (err) return res.status(500).json({error: err});
-            console.log(files.image.path.split("uploads/"));
+            console.log(files.image.path.split("uploads/").get[1]);
             uploadproduct.uploadproduct(fields.productid, files.image.path);
             res.status(200).json({uploaded: true, name: fields.iduser})
         });
