8e73945b3ccefeb2d952a284c40e25f658bdec9b
sega4 <sega4revenge@gmail.com>
2017-08-10 17:51:34 +0700

    asd

--- functions/fun_product.js
+++ functions/fun_product.js
@@ -1,154 +1,153 @@
 'use strict';
 
-const product = new require('../models/product');
-const bcrypt = new require('bcryptjs');
+const product = new require("../models/product");
 
 exports.allproduct = () =>
 
-    new Promise((resolve, reject) => {
+	new Promise((resolve, reject) => {
 
-        product.find({type: "1"})
-            .populate('user')
-            .then(products => {
+		product.find({type: "1"})
+			.populate("user")
+			.then(products => {
 
-                if (products.length === 0) {
+				if (products.length === 0) {
 
-                    reject({status: 404, message: 'Product Not Found !'});
+					reject({status: 404, message: "Product Not Found !"});
 
-                } else {
+				} else {
 
-                    return products;
+					return products;
 
-                }
-            })
+				}
+			})
 
-            .then(product => {
+			.then(product => {
 
 
-                resolve({status: 200, listproduct: product});
+				resolve({status: 200, listproduct: product});
 
-            })
+			})
 
-            .catch(err => reject({status: 500, message: 'Internal Server Error !'}));
+			.catch(err => reject({status: 500, message: "Internal Server Error !"}));
 
-    });
+	});
 
 exports.createproduct = (userid, prodctname, price, number, description, type) =>
 
-    new Promise((resolve, reject) => {
+	new Promise((resolve, reject) => {
 
-        let newproduct;
+		let newproduct;
 
-        console.log(type);
-        if (type === 1) {
-            console.log(price);
-            newproduct = new product({
-                user: userid,
-                productname: prodctname,
-                price: price,
-                number: number,
-                description: description,
-                type: type
-            });
-        } else {
-            console.log(price);
-            newproduct = new product({
-                user: userid,
-                productname: prodctname,
-                price: "",
-                number: number,
-                description: description,
-                type: type
-            });
-        }
-
-
-        newproduct.save()
+		console.log(type);
+		if (type === 1) {
+			console.log(price);
+			newproduct = new product({
+				user: userid,
+				productname: prodctname,
+				price: price,
+				number: number,
+				description: description,
+				type: type
+			});
+		} else {
+			console.log(price);
+			newproduct = new product({
+				user: userid,
+				productname: prodctname,
+				price: "",
+				number: number,
+				description: description,
+				type: type
+			});
+		}
 
 
-            .then(() => {
-                newproduct.populate('user', '_id name email images', function (err) {
-                    resolve({status: 201, message: 'product Registered Sucessfully !', product: newproduct})
-                });
-            })
+		newproduct.save()
 
 
-            .catch(err => {
+			.then(() => {
+				newproduct.populate("user", "_id name email images", function (err) {
+					resolve({status: 201, message: "product Registered Sucessfully !", product: newproduct});
+				});
+			})
 
-                if (err.code === 11000) {
+
+			.catch(err => {
+
+				if (err.code === 11000) {
 
 
-                    reject({status: 409, message: 'product Already Registered !'});
+					reject({status: 409, message: "product Already Registered !"});
 
-                } else {
-                    reject({status: 500, message: 'Internal Server Error !'});
-                    throw err;
+				} else {
+					reject({status: 500, message: "Internal Server Error !"});
+					throw err;
 
-                }
-            });
-    });
+				}
+			});
+	});
 
 exports.productdetail = (productid) =>
 
-    new Promise((resolve, reject) => {
+	new Promise((resolve, reject) => {
 
-        let ObjectId;
-        ObjectId = require('mongodb').ObjectID;
+		let ObjectId;
+		ObjectId = require("mongodb").ObjectID;
 
-        product.find({_id: ObjectId(productid)})
-            .populate('user')
-            .then(products => {
+		product.find({_id: ObjectId(productid)})
+			.populate("user")
+			.then(products => {
 
-                if (products.length === 0) {
+				if (products.length === 0) {
 
-                    reject({status: 404, message: 'Product Not Found !'});
+					reject({status: 404, message: "Product Not Found !"});
 
-                } else {
+				} else {
 
-                    return products[0];
+					return products[0];
 
-                }
-            })
+				}
+			})
 
-            .then(product => {
+			.then(product => {
 
 
-                resolve({status: 200, product: product});
+				resolve({status: 200, product: product});
 
-            })
+			})
 
-            .catch(err => reject({status: 500, message: 'Internal Server Error !'}));
+			.catch(err => reject({status: 500, message: "Internal Server Error !"}));
 
-    });
+	});
 
 exports.uploadproduct = (productid, image) =>
 
-    new Promise((resolve, reject) => {
+	new Promise((resolve, reject) => {
 
-        console.log(productid);
+		console.log(productid);
 
-        let ObjectId;
-        ObjectId = require('mongodb').ObjectID;
+		let ObjectId;
+		ObjectId = require("mongodb").ObjectID;
 
-        product.find({_id: ObjectId(productid)})
-            .populate('user')
-            .then(products => {
+		product.find({_id: ObjectId(productid)})
+			.populate("user")
+			.then(products => {
 
-                if (products.length === 0) {
+				if (products.length === 0) {
 
-                    reject({status: 404, message: 'User Not Found !'});
+					reject({status: 404, message: "User Not Found !"});
 
-                } else {
+				} else {
 
-                    return products[0];
+					return products[0];
 
-                }
-            })
+				}
+			})
 
-            .then(product => {
-                product.images.push(image);
-                product.save();
-            })
-            .catch(err => reject({status: 500, message: 'Internal Server Error !'}))
+			.then(product => {
+				product.images.push(image);
+				product.save();
+			})
+			.catch(err => reject({status: 500, message: "Internal Server Error !"}));
 
-    });
+	});
