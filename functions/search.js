e16b5374513dc9b8a3af836e29e586265ea6d791
sega4revenge <sega4revenge@gmail.com>
2017-08-12 17:23:32 +0700

    vinh

---
+++ functions/search.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const product = new require('../models/product');
+const bcrypt = new require('bcryptjs');
+
+exports.mSearch = (searchkey,location, category,typeArrange) =>
+
+	new Promise((resolve,reject) => {
+
+		product.find({ "productname": /^.*\$searchkey.*$/i , "location": /^.*\$location.*$/i , "category": /^.*\category.*$/i})
+
+			.then(product => {
+
+				if (product.length === 0) {
+
+					reject({ status: 404, message: 'User Not Found !' });
+
+				} else {
+					return product;
+				}
+			})
+	});
