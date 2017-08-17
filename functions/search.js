6c11fa86a961c55909a44bfa848499a3668bd1d4
sega4revenge <sega4revenge@gmail.com>
2017-08-17 21:11:08 +0700

    vinh

--- functions/search.js
+++ functions/search.js
@@ -4,7 +4,10 @@ const product = new require('../models/product');
 const bcrypt = new require('bcryptjs');
 
 exports.mSearch = (searchkey,location, category,typeArrange) =>
-
+	console.log("keysearch1 = " + searchkey);
+	console.log("category1 = " + category);
+	console.log("location1 = " + location);
+	console.log("typeArrange1 = " + typeArrange);
 	new Promise((resolve,reject) => {
 		if(typeArrange==0)
 		{
