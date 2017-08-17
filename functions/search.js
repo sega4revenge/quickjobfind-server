bfbf911329fdecb1ba7320b294e486f7e4dd9235
sega4revenge <sega4revenge@gmail.com>
2017-08-17 21:20:56 +0700

    vinh

--- functions/search.js
+++ functions/search.js
@@ -6,10 +6,6 @@ const bcrypt = new require('bcryptjs');
 exports.mSearch = (searchkey,location, category,typeArrange) =>
 
 	new Promise((resolve,reject) => {
-		console.log("keysearch1 = " + searchkey );
-		console.log("category1 = " + category );
-		console.log("location1 = " + location );
-		console.log("typeArrange1 = " + typeArrange);
 		if(typeArrange==0)
 		{
 			product.find( {productname: {  $regex :  searchkey  }, address : { $regex: location },category: category } ).sort({created_at: -1})
@@ -17,11 +13,11 @@ exports.mSearch = (searchkey,location, category,typeArrange) =>
 				.then(products => {
 
 					if (products.length === 0) {
-
+						console.log("nullllll = ");
 						reject({status: 404, message: "Product Not Found !"});
 
 					} else {
-
+						console.log("products = " + products);
 						return products;
 
 					}
@@ -49,7 +45,7 @@ exports.mSearch = (searchkey,location, category,typeArrange) =>
 					}
 				})
 				.then(product => {
-					resolve({status: 200,message: "123123 !" });//listproduct: product
+					resolve({status: 200, listproduct: product});
 
 				})
 				.catch(err => reject({status: 500, message: "Internal Server Error !"}));
@@ -71,7 +67,7 @@ exports.mSearch = (searchkey,location, category,typeArrange) =>
 					}
 				})
 				.then(product => {
-					resolve({status: 200, message: "123123!"});
+					resolve({status: 200, listproduct: product});
 
 				})
 				.catch(err => reject({status: 500, message: "Internal Server Error !"}));
@@ -93,7 +89,7 @@ exports.mSearch = (searchkey,location, category,typeArrange) =>
 					}
 				})
 				.then(product => {
-					resolve({status: 200, message: "123123!"});
+					resolve({status: 200, listproduct: product});
 
 				})
 				.catch(err => reject({status: 500, message: "Internal Server Error !"}));
