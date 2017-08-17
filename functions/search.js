e004d4eb4bab59c9d5ab9cbc4c161e8c07dd1806
sega4revenge <sega4revenge@gmail.com>
2017-08-17 09:18:02 +0700

    vinh

--- functions/search.js
+++ functions/search.js
@@ -7,7 +7,7 @@ exports.mSearch = (searchkey,location, category,typeArrange) =>
 
 	new Promise((resolve,reject) => {
 
-		product.find( { producname: /.*vấp/  } )//, category: category ,$text: { $search: searchkey+" "+location }
+		product.find( { producname: /.*Gò.*/  } )//, category: category ,$text: { $search: searchkey+" "+location }
 		//.find({
 		//	productname:  /^.*\$searchkey.*$/i,
 			//location: 	/^.*\$location.*$/i,
