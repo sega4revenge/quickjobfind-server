5506786809f2ea1e946f3c9fb3dd15283a9f26c9
sega4 <sega4revenge@gmail.com>
2017-08-17 21:50:06 +0700

    asd

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ const app        = express();
 const bodyParser = require('body-parser');
 const logger        = require('morgan');
 const router        = express.Router();
+var server = require('http').createServer(app);
 const port        = process.env.PORT || 8080;
-
+var io = require('socket.io')(server);
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
 require('./routes')(router);
 app.use('/api/v1', router);
-app.listen(port);
+
+server.listen(port, function () {
+	console.log('Server listening at port %d', port);
+});
+
 
 console.log(`App Runs on ${port}`);
 
