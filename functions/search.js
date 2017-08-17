c40ff10adb0aa4fbcb99c307a293287ea84ee434
sega4revenge <sega4revenge@gmail.com>
2017-08-17 21:08:55 +0700

    vinh

--- functions/search.js
+++ functions/search.js
@@ -8,11 +8,29 @@ exports.mSearch = (searchkey,location, category,typeArrange) =>
 	new Promise((resolve,reject) => {
 		if(typeArrange==0)
 		{
+			product.find( {productname: {  $regex :  searchkey  }, address : { $regex: location },category: category } ).sort({created_at: -1})
+				.populate("user")
+				.then(products => {
 
+					if (products.length === 0) {
+
+						reject({status: 404, message: "Product Not Found !"});
+
+					} else {
+
+						return products;
+
+					}
+				})
+				.then(product => {
+					resolve({status: 200, listproduct: product});
+
+				})
+				.catch(err => reject({status: 500, message: "Internal Server Error !"}));
 		}
 		if(typeArrange==1)
 		{
-			product.find( {productname: {  $regex :  " "  }, address : { $regex: " " },category: category } ).sort({created_at: -1})
+			product.find( {productname: {  $regex :  searchkey  }, address : { $regex: location },category: category } ).sort({created_at: -1})
 				.populate("user")
 				.then(products => {
 
