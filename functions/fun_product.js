2386b9f71a050e9c41d3f72997563a46f80b5e79
LINH <hohoanglinh190295@gmail.com>
2017-08-18 09:17:14 +0700

    BOSS LINH PUSH

--- functions/fun_product.js
+++ functions/fun_product.js
@@ -177,9 +177,9 @@ exports.productdetail = (productid) =>
 				path: "user comment",
 
 				// Get friends of friends - populate the 'friends' array for every friend
-				populate: {path: "user", select: "_id name photoprofile", sort : {time : -1}}
+				populate: {path: "user", select: "_id name photoprofile"}
 			})
-
+			.sort({time : -1})
 			.then(products => {
 
 				if (products.length === 0) {
