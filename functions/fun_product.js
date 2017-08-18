625d49072d179fd7e11724a5c2fcb650031535c3
LINH <hohoanglinh190295@gmail.com>
2017-08-18 09:27:21 +0700

    BOSS LINH PUSH

--- functions/fun_product.js
+++ functions/fun_product.js
@@ -175,7 +175,7 @@ exports.productdetail = (productid) =>
 		product.find({_id: ObjectId(productid)})
 			.populate({
 				path: "user comment",
-				options: { sort: { 'time': 1 } },
+				options: { sort: { 'time': -1 } },
 				// Get friends of friends - populate the 'friends' array for every friend
 				populate: {path: "user", select: "_id name photoprofile" }
 			})
