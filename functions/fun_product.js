48d4ff7ea1ec2756d797d62b3d97043ad408d927
LINH <hohoanglinh190295@gmail.com>
2017-08-18 10:33:10 +0700

    BOSS LINH PUSH

--- functions/fun_product.js
+++ functions/fun_product.js
@@ -117,7 +117,7 @@ exports.refreshcomment = (productid) =>
 					.then(comment => {
 
 
-						resolve({status: 201,comment: comment});
+						resolve({status: 201, message: "Comment Sucessfully !", comment: comment});
 
 					})
 
@@ -161,10 +161,29 @@ exports.addcomment = (userid, productid, content, time) =>
 					function (err, model) {
 						console.log(err);
 					}
-				)
+				);
+				let ObjectId;
+				ObjectId = require("mongodb").ObjectID;
+				comment.find({productid: ObjectId(productid)})
+					.populate("user", "_id name photoprofile" )
+					.then(comments => {
 
-						resolve({status: 201, message: "Comment Sucessfully !"});
+						if (comments.length === 0) {
 
+							reject({status: 404, message: "Product Not Found !"});
+
+						} else {
+
+							return comments;
+
+						}
+					})
+					.then(comment => {
+
+
+						resolve({status: 201, message: "Comment Sucessfully !", comment: comment});
+
+					});
 
 
 			})
