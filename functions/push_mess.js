36f6dc57956c32790fe29ad93e009f750f5af3ef
LINH <hohoanglinh190295@gmail.com>
2017-08-22 15:46:35 +0700

    BOSS LINH PUSH

--- functions/push_mess.js
+++ functions/push_mess.js
@@ -6,7 +6,7 @@ exports.push_mess = (msg,deviceId) =>
 
     new Promise((resolve, reject) => {
 		var m = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-			to : deviceId,
+			registration_ids : ["dT0HzvO4J-g:APA91bHHsYDB_9A5rfo7lmoRu0tpHN-AOA6629YaYycd2KqkxyUjog93RRp-JiK9VvPhYNJlMwWpilXI3aFsqq1rh24dRkXZ6YkcYjhbgalOuCfpRh9pXtSGTh44IBNpXtvPTnEtf3OB","frR-tTgnn-E:APA91bGdaf7qqI4oVAC4qEMgjYqU8NoIob2B8BaXnJnSVaw0bDnV0qVhzBfaRbMZ1xKKTxFH4rn8GOs46pGRE8x_IrRH-lBRXR0wMYp-LKVtUIsnf4IHaHIbbkC7mo-q9hvbVFPQ9pvv"],
 
 			data: {
 				message: msg
