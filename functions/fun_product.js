0e44bfc319f9916ff502084f49d56fdf12efd954
sega4revenge <sega4revenge@gmail.com>
2017-08-23 19:43:54 +0700

    vinh

--- functions/fun_product.js
+++ functions/fun_product.js
@@ -48,9 +48,11 @@ exports.informationUser = userid =>
 					let ObjectId;
 					ObjectId = require('mongodb').ObjectID;
 					user.find({ _id: ObjectId(userid)})
-					.then(user => {
-							resolve({status: 202, user: user });
-					})
+						.exec(function (err, post) {
+							if(err) throw err;
+							console.log(post);
+						})
+					 .then(users => resolve(users[0]))
 					.catch(err => reject({status: 500, message: "Internal Server Error !"}));
 
 				} else {
